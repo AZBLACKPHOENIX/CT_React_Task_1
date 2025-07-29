533b4fdc0f6bc8005c89375f045d161320fd47c4
AZBLACKPHOENIX <unlimitednigami@gmail.com>
2025-07-29 14:29:42 +0200

    Fix viteconfig Error

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: './', 
   plugins: [react()],
   preview: {
     host: true,
-    port: process.env.PORT || 4173,
+    port: Number(process.env.PORT) || 4173,
     allowedHosts: ['ct-react-task-1-wsjr.onrender.com']
   }
 })
