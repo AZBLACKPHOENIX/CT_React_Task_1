25bff033f1ce7deb5d065cb6ea85585ebe4f151e
AZBLACKPHOENIX <unlimitednigami@gmail.com>
2025-07-29 15:07:15 +0200

    Config

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
   build: {
-    outDir: 'dist',
-  },
-  preview: {
-    host: true,
-    port: Number(process.env.PORT) || 4173,
-    allowedHosts: ['ct-react-task-1-wsjr.onrender.com']
+    outDir: 'dist'
   }
 })
